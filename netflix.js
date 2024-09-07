2d48a6859ecff840ae6d242c3dfa3310de792a0e
Aniruddh012 <aniruddhprajapati213@gmail.com>
2024-09-07 17:13:56 +0530

    add the netflix

---
+++ netflix.js
@@ -0,0 +1,15 @@
+let coment=document.querySelector(".comment");
+let sign= document.querySelector(".button1");
+let titl = document.querySelector(".title"); 
+
+
+sign.addEventListener("click",()=>{
+    for(let i=0;i<10;i++){
+
+        console.log(i+"the nukbr is ");
+    }
+
+    console.log("thanks");
+    coment.innerText="aniruddh";
+});
+
